6e28cd36df7f712054ed46f8da7d7c01122d346a
Eva Schuh <eva.schuh@ag.ch>
2024-06-19 10:09:49 +0200

    test: add test

--- src/LyrxParser.spec.ts
+++ src/LyrxParser.spec.ts
@@ -1,5 +1,15 @@
-import { expect, test } from 'vitest';
+import { expect, test } from "vitest";
+import fs from "fs";
+import { LyrxParser } from "./LyrxParser";
 
-test('adds 1 + 1 to equal 2', () => {
+test("adds 1 + 1 to equal 2", () => {
   expect(1 + 1).toBe(2);
+});
+
+test("Should", async () => {
+  var lyrxParser = new LyrxParser();
+  const lyxText = fs.readFileSync("./data/lyrx/test.lyrx", "utf8");
+  const lyrx = JSON.parse(lyxText);
+  const { output: geoStylerStyle } = await lyrxParser.readStyle(lyrx);
+  expect(geoStylerStyle).toBeDefined();
 });
