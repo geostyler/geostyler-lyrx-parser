69e33c82a9064e847e1e96176a76c85d42515ad4
Eva Schuh <eva.schuh@ag.ch>
2025-06-03 11:22:52 +0200

    test: remove console log

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -9,6 +9,5 @@ export async function loadGeostylerStyle(
   const lyrxParser = new LyrxParser();
   const lyrx: CIMLayerDocument = JSON.parse(fs.readFileSync(filePath, "utf8"));
   let style = await lyrxParser.readStyle(lyrx);
-  console.log(JSON.stringify(style, null, 2));
   return style;
 }
