28b8d1d68cd8629b00bec8f53ccf5e996c6aa5c1
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2026-04-28 19:01:02 +0200

    fix: remove unused packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,32 @@
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import "@terrestris/eslint-config-typescript";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 const compat = new FlatCompat({
   baseDirectory: dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends('@terrestris/eslint-config-typescript'), {
-  files: ['**/*.ts', '**/*.tsx'],
-  languageOptions: {
-    parser: tsParser,
-    parserOptions: {
-      project: ['tsconfig.json'],
-      tsconfigRootDir: dirname,
-    }
+export default [
+  ...compat.extends("@terrestris/eslint-config-typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ["tsconfig.json"],
+        tsconfigRootDir: dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/member-ordering": "off",
+      "@stylistic/quotes": ["warn", "double"],
+    },
   },
-  rules: {
-    '@typescript-eslint/member-ordering': 'off',
-    '@stylistic/quotes': ['warn', 'double']
-  }
-}];
+];
